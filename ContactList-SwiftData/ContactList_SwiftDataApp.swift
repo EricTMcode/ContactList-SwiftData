a15cbe0fd4c3041d49b1adcecb2cc8ffcd7c4125
Eric <ebagnaro@gmail.com>
2024-02-02 14:00:34 +0100

    Initial Commit

---
+++ ContactList-SwiftData/ContactList_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftDataApp.swift
+//  ContactList-SwiftData
+//
+//  Created by Eric on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
